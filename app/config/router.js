31db491e02b99ed55f6b3bbdb70183a01132a673
Quentin Jacquet <q.jacquet@gmail.com>
2018-07-02 19:44:26 +0200

    first commit : need to fix items onPress()

---
+++ app/config/router.js
@@ -0,0 +1,91 @@
+import React, { Component } from 'react';
+import { Dimensions, Platform } from 'react-native';
+import { createMaterialTopTabNavigator, createStackNavigator } from 'react-navigation';
+import { Icon } from 'react-native-elements';
+
+import Bookcase from '../screens/Bookcase';
+import Explore from '../screens/Explore';
+import AddBook from '../screens/AddBook';
+import Lists from '../screens/Lists';
+import Profile from '../screens/Profile';
+import EditBook from '../screens/EditBook';
+
+let screen = Dimensions.get('window');
+
+export const Tabs = createMaterialTopTabNavigator({
+  'Bookcase': {
+    screen: Bookcase,
+    navigationOptions: {
+      tabBarLabel: 'Bookcase',
+      tabBarIcon: ({ tintColor }) => <Icon name="open-book" type="entypo" size={28} color={tintColor} />
+    },
+  },
+  'Explore': {
+    screen: Explore,
+    navigationOptions: {
+      tabBarLabel: 'Explore',
+      tabBarIcon: ({ tintColor }) => <Icon name="ios-map-outline" type="ionicon" size={28} color={tintColor} />
+    },
+  },
+  'Add Book': {
+    screen: AddBook,
+    navigationOptions: {
+      tabBarLabel: 'Add Book',
+      tabBarIcon: ({ tintColor }) => <Icon name="ios-add-circle-outline" type="ionicon" size={28} color={tintColor} />
+    },
+  },
+  'Lists': {
+    screen: Lists,
+    navigationOptions: {
+      tabBarLabel: 'Lists',
+      tabBarIcon: ({ tintColor }) => <Icon name="list" type="entypo" size={28} color={tintColor} />
+    },
+  },
+  'My Profile': {
+    screen: Profile,
+    navigationOptions: {
+      tabBarLabel: 'Profile',
+      tabBarIcon: ({ tintColor }) => <Icon name="ios-person-outline" type="ionicon" size={28} color={tintColor} />
+    },
+  },
+});
+
+export const BookcaseStack = createStackNavigator({
+  Bookcase: {
+    screen: Bookcase,
+    navigationOptions: ({navigation}) => ({
+      header: null,
+    }),
+  },
+  EditBook: {
+    screen: EditBook,
+    navigationOptions: ({navigation}) => ({
+      header: null,
+      tabBarVisible: false,
+      gesturesEnabled: false
+    }),
+  },
+});
+
+export const createRootNavigator = () => {
+  return createStackNavigator(
+    {
+      BookcaseStack: {
+        screen: BookcaseStack,
+        navigationOptions: {
+          gesturesEnabled: false
+        }
+      },
+      Tabs: {
+        screen: Tabs,
+        navigationOptions: {
+          gesturesEnabled: false
+        }
+      }
+    },
+    {
+      headerMode: "none",
+      mode: "modal"
+    }
+  );
+};
